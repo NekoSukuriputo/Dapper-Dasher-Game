26f97ce1b7e3b46de5fa9883288b43e21e1d37a8
abdul <abdul.rahmat@360kredi.id>
2024-03-07 17:32:51 +0700

    29. grafity and accelerator

--- dasher.cpp
+++ dasher.cpp
@@ -9,6 +9,9 @@ int main()
     // init window
     InitWindow(windowWidth, windowHeight, "Dapper Dasher");
 
+    // acceleration due to grafity (pixels/frame)/frame
+    const int gravity{1};
+
     // rectangle dimentions
     const int width{50};
     const int height{80};
@@ -21,23 +24,36 @@ int main()
 
     while (!WindowShouldClose())
     {
-        // start drawing
+         // start drawing
         BeginDrawing();
         ClearBackground(WHITE);
+
+        // perform ground check
+        if (posY >= windowHeight - height)
+        {
+            // rectangle is on the ground
+            velocity = 0;
+        }
+        else
+        {
+            // rectangle is in the air
+            velocity += gravity;
+        }
 
         if (IsKeyPressed(KEY_SPACE))
         {
             velocity -= 10;
         }
 
+        // update the position
         posY += velocity;
 
         DrawRectangle(windowWidth/2, posY, width, height, BLUE);
 
         // stop drawing
         EndDrawing();
+
     }
 
     CloseWindow();
-    
 }
