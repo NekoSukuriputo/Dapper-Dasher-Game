307598fa6c1d18bd777aa4c5e1301436d75d46f6
abdul <abdul.rahmat@360kredi.id>
2024-03-07 16:48:27 +0700

    28. velocity and jumping

--- dasher.cpp
+++ dasher.cpp
@@ -9,6 +9,13 @@ int main()
     // init window
     InitWindow(windowWidth, windowHeight, "Dapper Dasher");
 
+    // rectangle dimentions
+    const int width{50};
+    const int height{80};
+
+    int posY{windowHeight - height};
+    int velocity{0};
+
     // set fps
     SetTargetFPS(60);
 
@@ -18,6 +25,14 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
+
+        posY += velocity;
+
+        DrawRectangle(windowWidth/2, posY, width, height, BLUE);
 
         // stop drawing
         EndDrawing();
