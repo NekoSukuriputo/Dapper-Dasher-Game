d4f6511ba888e802e018a4d960d0875041b1c295
abdul <abdul.rahmat@360kredi.id>
2024-03-07 17:54:04 +0700

    30. preventing air jumping

--- dasher.cpp
+++ dasher.cpp
@@ -16,6 +16,11 @@ int main()
     const int width{50};
     const int height{80};
 
+    //is ractacle in the air
+    bool isInAir{};
+    //jump velocity
+    const int jumpVel{-22};
+
     int posY{windowHeight - height};
     int velocity{0};
 
@@ -33,16 +38,19 @@ int main()
         {
             // rectangle is on the ground
             velocity = 0;
+            isInAir = false;
         }
         else
         {
             // rectangle is in the air
             velocity += gravity;
+            isInAir = true;
         }
 
-        if (IsKeyPressed(KEY_SPACE))
+        // jump check
+        if (IsKeyPressed(KEY_SPACE) && !isInAir)
         {
-            velocity -= 10;
+            velocity += jumpVel;
         }
 
         // update the position
