452fedb354de67f082a836f35c95484a1b42aa29
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2024-03-09 17:28:35 +0700

    52. Drawing Midground And Foreground

--- dasher.cpp
+++ dasher.cpp
@@ -55,14 +55,14 @@ int main()
     {
         nebulae[i].rec.x = 0.0;
         nebulae[i].rec.y = 0.0;
-        nebulae[i].rec.width = nebula.width/8;
-        nebulae[i].rec.height = nebula.height/8;
-        nebulae[i].pos.y = windowDimensions[1] - nebula.height/8;
+        nebulae[i].rec.width = nebula.width / 8;
+        nebulae[i].rec.height = nebula.height / 8;
+        nebulae[i].pos.y = windowDimensions[1] - nebula.height / 8;
         nebulae[i].frame = 0;
         nebulae[i].runningTime = 0.0;
         nebulae[i].updateTime = 0.0;
 
-        nebulae[i].pos.x =  windowDimensions[0] + i * 300;
+        nebulae[i].pos.x = windowDimensions[0] + i * 300;
     }
 
     // nebula X velocity (pixels/second)
@@ -71,14 +71,14 @@ int main()
     // scarfy variables
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
     AnimData scarfyData;
-    scarfyData.rec.width = scarfy.width/6;
+    scarfyData.rec.width = scarfy.width / 6;
     scarfyData.rec.height = scarfy.height;
     scarfyData.rec.x = 0;
     scarfyData.rec.y = 0;
-    scarfyData.pos.x = windowDimensions[0]/2 - scarfyData.rec.width/2;
+    scarfyData.pos.x = windowDimensions[0] / 2 - scarfyData.rec.width / 2;
     scarfyData.pos.y = windowDimensions[1] - scarfyData.rec.height;
     scarfyData.frame = 0;
-    scarfyData.updateTime = 1.0/12.0;
+    scarfyData.updateTime = 1.0 / 12.0;
     scarfyData.runningTime = 0.0;
 
     // is the rectanlge in the air?
@@ -90,6 +90,10 @@ int main()
 
     Texture2D background = LoadTexture("textures/far-buildings.png");
     float bgX{};
+    Texture2D midground = LoadTexture("textures/back-buildings.png");
+    float mgX{};
+    Texture2D foreground = LoadTexture("textures/foreground.png");
+    float fgX{};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -103,16 +107,40 @@ int main()
 
         // Scroll background
         bgX -= 20 * dT;
-        if (bgX <= -background.width*2)
+        if (bgX <= -background.width * 2)
         {
             bgX = 0.0;
+        }
+        // Scroll the midground
+        mgX -= 40 * dT;
+        if (mgX <= -midground.width * 2)
+        {
+            mgX = 0.0;
+        }
+        // Scroll the midground
+        fgX -= 80 * dT;
+        if (fgX <= -foreground.width * 2)
+        {
+            fgX = 0.0;
         }
 
         // draw the background
         Vector2 bg1Pos{bgX, 0.0};
         DrawTextureEx(background, bg1Pos, 0.0, 2.0, WHITE);
-        Vector2 bg2Pos{bgX + background.width*2, 0.0};
+        Vector2 bg2Pos{bgX + background.width * 2, 0.0};
         DrawTextureEx(background, bg2Pos, 0.0, 2.0, WHITE);
+
+        // draw the midground
+        Vector2 mg1Pos{mgX, 0.0};
+        DrawTextureEx(midground, mg1Pos, 0.0, 2.0, WHITE);
+        Vector2 mg2Pos{mgX + midground.width * 2, 0.0};
+        DrawTextureEx(midground, mg2Pos, 0.0, 2.0, WHITE);
+
+        // draw the foreground
+        Vector2 fg1Pos{fgX, 0.0};
+        DrawTextureEx(foreground, fg1Pos, 0.0, 2.0, WHITE);
+        Vector2 fg2Pos{fgX + foreground.width * 2, 0.0};
+        DrawTextureEx(foreground, fg2Pos, 0.0, 2.0, WHITE);
 
         // perform ground check
         if (isOnGround(scarfyData, windowDimensions[1]))
@@ -133,7 +161,6 @@ int main()
         {
             velocity += jumpVel;
         }
-
 
         for (int i = 0; i < sizeOfNebulae; i++)
         {
@@ -170,5 +197,7 @@ int main()
     UnloadTexture(scarfy);
     UnloadTexture(nebula);
     UnloadTexture(background);
+    UnloadTexture(midground);
+    UnloadTexture(foreground);
     CloseWindow();
 }
